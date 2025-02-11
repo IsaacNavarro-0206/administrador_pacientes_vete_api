04cc0baef2ae258f8109eb03126a7f0cc357f4fe
Isaac David Navarro Hernández <50563092+IsaacNavarro-0206@users.noreply.github.com>
2025-02-11 11:46:05 -0500

    feat: se agregó dominio al correo

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -15,7 +15,7 @@ const emailReigstro = async datos => {
 
     // Enviar email
     const info = await transport.sendMail({
-        from: '"APV - Administrador de pacientes de veterinaria" <no-reply@apv.com>',
+        from: '"APV - Administrador de pacientes de veterinaria" <no-reply@apv.life>',
         to: email,
         subject: "Comprueba tu cuenta en APV",
         text: "Comprobar tu cuenta",
